484559138b6a37303892f5df956ada01e89e0da8
jlazala3102 <j.lazala3102@gmail.com>
2025-01-14 17:56:07 -0600

    content updates

--- react-website-1/src/App.js
+++ react-website-1/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -9,7 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
+          <Route path='/' exact element={<Home />}> </Route>
+          <Route path='/services'  element={<Services />}> </Route>
+          <Route path='/products'  element={<Products />}> </Route>
+          <Route path='/sign-up'  element={<SignUp />}> </Route>
         </Routes>
       </Router>
     </>
